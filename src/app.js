a5b2756f237dee3a99dda188e9f706a9b472c577
Wilker Mizael <107046187+wilkermizael@users.noreply.github.com>
2023-07-13 19:55:11 -0300

    feat:Envio dos produtos via requisição

--- src/app.js
+++ src/app.js
@@ -2,10 +2,11 @@ import express from "express"
 import cors from "cors"
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv"
+import ships from "./products/products.js"
 
 const app = express();
 app.use(express.json());
-app.use(cors);
+app.use(cors());
 dotenv.config()
 
 // conectando ao banco
@@ -13,11 +14,34 @@ const mongoClient = new MongoClient(process.env.DATABASE_URL);
 let db;
 
 mongoClient.connect()
-	.then(() => {
-        console.log("MongoDB conected")
-        db = mongoClient.db()})
-	.catch((err) => console.log(err.message))
+        .then(() => {
+                console.log("MongoDB conected")
+                db = mongoClient.db()
+        })
+        .catch((err) => console.log(err.message))
 
 
+app.get('/products', async (req, res) => {
+        
+        try {
+                const list = await db.collection('products').find().toArray()
+               
+                if (list.length === 0) {
+                        await db.collection('products').insertMany(ships)
+                        return res.status(200).send(ships)
+                        
+                } else {
+                       
+                        return res.status(200).send(list)
+                }
+
+
+
+        } catch (error) {
+                return res.status(500).send(error)
+        }
+})
 // servidor rodando na porta 5000
-app.listen(process.env.PORT, () => console.log(`Running server on port ${process.env.PORT}`))
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`Running server on port ${port}`))
+
