ab52001e2cb3643df960369693ec50e9a489efce
Wilker Mizael <107046187+wilkermizael@users.noreply.github.com>
2023-07-13 20:01:18 -0300

    feat:Envio dos produtos para usuario via requisição

--- src/app.js
+++ src/app.js
@@ -35,8 +35,6 @@ app.get('/products', async (req, res) => {
                         return res.status(200).send(list)
                 }
 
-
-
         } catch (error) {
                 return res.status(500).send(error)
         }
