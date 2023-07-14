18072efc13948a38ce343e999a6e3bf5b08fe2c9
Gabriel Oliveira <gabriel_lopeslomeu@hotmail.com>
2023-07-14 17:33:28 -0300

    feat: versão inicial gabriel

--- src/app.js
+++ src/app.js
@@ -19,5 +19,14 @@ mongoClient.connect()
 	.catch((err) => console.log(err.message))
 
 
+app.get("/carrinho", async (req,res)=>{
+        try {
+                res.send("ok")
+        } catch (error) {
+                res.status(500).send(error.message)
+        }
+})
+
 // servidor rodando na porta 5000
-app.listen(process.env.PORT, () => console.log(`Running server on port ${process.env.PORT}`))
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`Running server on port ${port}`))
